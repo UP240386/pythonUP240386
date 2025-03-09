93da2826023054eb75b3ce4c02cf1376228d9abe
UP240386 <up240386@alumnos.upa.edu.mx>
2025-03-09 15:19:33 -0600

    del_4_al_6

---
+++ 04_Day/01_Exercises.py
@@ -0,0 +1,116 @@
+#num_1
+print('Thirty' + ' Days' + ' Of' + ' Python') 
+
+#num_2
+print('Coding' + ' For' + ' All') 
+
+#num_3
+company = 'Coding For All'
+
+#num_4
+print(company)
+
+#num_5
+print(len(company))
+
+#num_6
+print(company.upper())
+
+#num_7
+print(company.lower())
+
+#num_8
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+#num_9
+print(company[7:14])
+
+#num_10
+print(company.index('Coding'))
+print(company.find('Coding'))
+
+#num_11
+print(company.replace('Coding ', 'Python '))
+
+#num_12
+print('Python For Everyone'.replace('Everyone', 'All'))
+
+#num_13
+print(company.split())
+
+#num_14
+print("Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon".split(','))
+
+#num_15
+print(company[0])
+
+#num_16
+print(len(company) - 1)
+
+#num_17
+print(company[10])
+
+#num_18
+words = "Python For Everyone".split()
+print(words[0][0] + words[1][0] + words[2][0])
+
+#num_19
+words = 'Coding For All'.split()
+print(words[0][0] + words[1][0] + words[2][0])
+
+#num_20
+print("Coding For All".index('C'))
+
+#num_21
+print("Coding For All".index('F'))
+
+#num_22
+print("Coding For All People".rfind('l'))
+
+#num_23
+print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+
+#num_24
+print('You cannot end a sentence with because because because is a conjunction'.rfind('because'))
+
+#num_25
+print('You cannot end a sentence with because because because is a conjunction'.replace('because because because', ''))
+
+#num_26
+print('You cannot end a sentence with because because because is a conjunction'.find('because'))
+
+#num_27
+#Es lo mismo que la 25
+print('You cannot end a sentence with because because because is a conjunction'.replace('because because because', ''))
+
+#num_28
+print('Coding For All'.startswith('Coding'))
+
+#num_29
+print('Coding For All'.endswith('coding'))
+
+#num_30
+print('   Coding For All      '.strip('   '))
+
+#num_31
+print('30DaysOfPython'.isidentifier())
+print('thirty_days_of_python'.isidentifier())
+
+#num_32
+print('-'.join(['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']))
+
+#num_33
+print('I am enjoying this challenge.\nI just wonder what is next.')
+
+#num_34
+print('Name\t Age\tCountry\t City\nRishabh\t 250\tFinland\t Helsinki')
+
+#num_35
+print('radius =', 10)
+print('area =', 3.14, '* radius **', 2)
+print('The area of a circle with radius', 10, 'is', 314, 'meters square.')
+
+#num_36
+print('8 + 6 = 14\n8 - 6 = 2\n8 * 6 = 48\n8 / 6 = 1.33\n8 % 6 = 2\n8 // 6 = 1\n8 ** 6 = 262144')
